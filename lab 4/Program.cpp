fb4a9f9d6bd156f33b8cb44c2c62471e5d390d1b
Loki0Moki <124599231+Loki0Moki@users.noreply.github.com>
2023-12-02 23:56:12 +0600

    Add files via upload

---
+++ lab 4/Program.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#define max 5
+using namespace std;
+
+int Func1(int x, int y){
+     return x+y;
+}
+
+int Func2(int x, int y){
+     return x-y;
+}
+
+void Func3(int p){
+
+}
+
+void Func3(int p){
+
+}
+int Func4(int p){
+
+}
+
+int main(){
+    int a=5, b=3, c=10;
+    cout<<"Trade";
+    int d, e;
+    c = a + b;
+    d = c - 2;
+    e = d * 6 ;
+    a = Addition(b, c);
+    d = Substract(a, 10);
+    return 0;
+}
